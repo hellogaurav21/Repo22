1f479d56d0c327f5eab79d722ce4f65083cf2ce3
hellogaurav21 <59482605+hellogaurav21@users.noreply.github.com>
2026-02-26 17:23:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# 1. Define the Backend (Where the state file lives)
 terraform {
   required_providers {
     azurerm = {
@@ -8,20 +7,18 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "YOUR_STORAGE_RG_NAME" # Change this to the RG where your storage account is
+    resource_group_name  = "central-rg" # Change this to the RG where your storage account is
     storage_account_name = "tfgithublab251251"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
-# 2. Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-# 3. Create a simple Resource Group for testing
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg1" {
   name     = "rg-terraform-github-test"
-  location = "East US" # You can change this to "Central India" or any other region
+  location = "East US"
 }
