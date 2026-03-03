33558c8243945658fcf5db52ffb2e8c6f883e121
hellogaurav21 <59482605+hellogaurav21@users.noreply.github.com>
2026-03-03 18:11:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,8 @@ resource "azurerm_resource_group" "rg2" {
   name     = "rg2-terraform-github-test"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg3-terraform-github-test"
+  location = "East US"
+}
